9f7b12f8a63d4daae3f7752acc17e5ba240d8e77
Kiprey <HeyKiprey@outlook.com>
2019-10-02 12:06:30 +0800

    将提交的数据插入数据库，完成后端工作

--- Lession_2019-09-28/enroll.php
+++ Lession_2019-09-28/enroll.php
@@ -11,26 +11,28 @@
         $commonEmail = $_POST["commonEmail"];
         $whatSexToLove = $_POST["whatSexToLove"];
 
-        $mysql_server_name='localhost';
+        $mysql_server_URL='localhost';
         $mysql_username='root';
         $mysql_password='';
-        $mysql_datebase='MSC_Web_Learning';
+        $mysql_datebase='msc_web_learning';
         $mysql_table='testtable';
 
-        $sql_connect=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password, $mysql_datebase);
-        if(!$sql_connect)
-            exit ("连接数据库失败！");
+        $sql_connect=mysqli_connect($mysql_server_URL,$mysql_username,$mysql_password, $mysql_datebase);
+        if(mysqli_connect_errno($sql_connect))
+            exit ("连接数据库失败:".mysqli_connect_error());
 
-        //在此处不做插入数据的判断,因为侧重点不在这里
-        $sqlStr="INSERT INTO $mysql_table VALUES ($trueName, $sex, $vitalEmail, $callNum,
-        $internDepartment, $internPosition, $phoneNum, $commonEmail, $whatSexToLove)";
-        //echo $sqlStr;
+        //在此处不做插入数据的判断,因为此代码侧重点不在这里
+        //这个SQL语句快把我搞死了   `和'的区别
+        $sqlStr="INSERT INTO `$mysql_table` (`trueName`, `sex`, `vitalEmail`, `callNum`, `internDepartment`, `internPosition`, `phoneNum`, `commonEmail`, `whatSexToLove`) 
+            VALUES ('$trueName', '$sex', '$vitalEmail', '$callNum', '$internDepartment', '$internPosition', '$phoneNum', '$commonEmail', '$whatSexToLove')";
+
         if(mysqli_query($sql_connect,$sqlStr))
         {
             echo "报名成功！";
         }
         else
-           exit ("提交报名表失败！");
+            echo "提交报名表失败！";
+        mysqli_close($sql_connect);
     }
     else
         exit ("你想干啥？？？");
